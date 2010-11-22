97a56ac5154886769075845b8d0f9b0ddcd274a0
Pete Muir <pmuir@bleepbleep.org.uk>
2010-11-22 14:13:00 +0000

    seam specific config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ### Add your list of permitted repositories shere
-ALLOWED_REPOS = ['https://github.com/USER1/PROJECT1', 'https://github.com/USER2/PROJECT2'] 
+ALLOWED_REPOS = [] 
 ### Alternatively, add your list of permitted organisations here
-ALLOWED_ORGS = ['ORG1', 'ORG2']
+ALLOWED_ORGS = ['seam', 'weld']
 ### Add your sender email.  Remember, senders need to be admins on your AppEngine account.
-EMAIL_FROM="Me <me@company.com>"
+EMAIL_FROM="Pete Muir <pmuir@bleepbleep.org.uk>"
 
